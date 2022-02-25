1b91e53d35d8c767e1ffd4b1e59be0fb7e1b0ed3
James Wu <jameswu62@yahoo.co.nz>
2022-02-26 12:03:11 +1300

    re structure project

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 
 int main(int argc, char** argv)
 {
-    std::printf("my application\n\r");
+    // dummy code to get rid of unused parameter warning
+    (void)argv;
+    (void)argc;
+
+    std::printf("running my application...\n\r");
     return EXIT_SUCCESS;
 }
