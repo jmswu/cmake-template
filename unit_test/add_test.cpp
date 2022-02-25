37c7824cbb8da1e9856e56cb5838a035c109ae61
James Wu <jameswu62@yahoo.co.nz>
2022-02-26 12:51:26 +1300

    added another test

--- unit_test/add_test.cpp
+++ unit_test/add_test.cpp
@@ -1,9 +1,28 @@
 #include "gtest/gtest.h"
 #include "add.hpp"
 
-namespace
+namespace 
 {
-    TEST(AddTest, positiveNumber)
+    class AddTest : public::testing::Test
+    {
+    public:
+    private:
+        
+    protected:
+        AddTest(){}
+
+        virtual void SetUp()
+        {
+
+        }
+
+        virtual void TearDown()
+        {
+
+        }
+    };
+
+    TEST_F(AddTest, positiveNumber)
     {
         ASSERT_EQ(100, add(10, 90));
         ASSERT_EQ(123, add(100, 23));
